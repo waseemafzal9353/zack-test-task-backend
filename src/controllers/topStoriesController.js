2eef94ace71f7446233b15aa30bd8962b9860d19
waseemafzal <waseem.afzal9353@gmail.com>
2023-10-26 04:03:48 -0700

    optimized routes to dynamicity

--- src/controllers/topStoriesController.js
+++ src/controllers/topStoriesController.js
@@ -3,8 +3,9 @@ import axios from "axios"
 // Fetch Top Stories
 const fetchWorldTopStoriesController = async (req, res) => {
     const section = req.params.section
+    const url = `${process.env.API_URL}/${section}.json?api-key=${process.env.KEY}`
 try {
-    const worldTopStories = await axios.get(`${process.env.API_URL}/${section}.json?api-key=${process.env.KEY}`);
+    const worldTopStories = await axios.get(url);
     res.json(worldTopStories.data)
 } catch (error) {
     console.error('Error:', error);
