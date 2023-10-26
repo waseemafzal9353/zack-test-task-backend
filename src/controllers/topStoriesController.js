c14424a19b741d7e0173662aff93bd53c6828901
waseemafzal <waseem.afzal9353@gmail.com>
2023-10-26 04:10:48 -0700

    optimized controller

--- src/controllers/topStoriesController.js
+++ src/controllers/topStoriesController.js
@@ -5,8 +5,8 @@ const fetchTopStoriesController = async (req, res) => {
     const section = req.params.section
     const url = `${process.env.API_URL}/${section}.json?api-key=${process.env.KEY}`
 try {
-    const worldTopStories = await axios.get(url);
-    res.json(worldTopStories.data)
+    const topStories = await axios.get(url);
+    res.json(topStories.data)
 } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal server error' });
